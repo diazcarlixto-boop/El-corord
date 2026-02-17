814837fccd433974cd0b390d5f47ef7af3392321
diazcarlixto-boop <diazcarlixto@gmail.com>
2026-02-16 22:12:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 
-document.getElementById('btn-aventura').addEventListener('click', function() {
-    document.getElementById('contenido').innerHTML = `
-        <img src="tu-imagen.jpg" alt="Aventura">
-        <p>Descripción de Aventura</p>
-    `;
-});
