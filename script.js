c9f9dc9a41d5e6725661e3c92aff64a606a28363
diazcarlixto-boop <diazcarlixto@gmail.com>
2026-02-17 20:57:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", function() {
+    document.getElementById("navbar")
+    .classList.toggle("scrolled", window.scrollY > 50);
+});
 
+function sendWhatsApp(tour){
+    let message = `Hola, mi nombre es ____. Deseo reservar el tour ${tour}. Por favor envíenme más información.`;
+    window.open(`https://wa.me/18493425653?text=${encodeURIComponent(message)}`,'_blank');
+}
